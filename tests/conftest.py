e433507de9ad4fb663ee012165848bc2bc88c5c8
Rui Barbosa <barduinor@gmail.com>
2025-03-24 15:33:25 -0400

    initial commit

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+from box_sdk_gen import BoxClient
+from src.langchain_box_agent.box.box_authentication import (
+    get_ccg_client,
+    # get_oauth_client,
+)
+
+
+# @pytest.fixture
+# def box_client_auth() -> BoxClient:
+#     return get_oauth_client()
+
+
+@pytest.fixture
+def box_client_ccg() -> BoxClient:
+    return get_ccg_client()
---
+++ tests/test_box_auth.py
@@ -0,0 +1,14 @@
+from box_sdk_gen import BoxClient, User
+
+
+def test_get_ccg_client(box_client_ccg: BoxClient):
+    from dotenv import load_dotenv
+
+    load_dotenv()
+    client = box_client_ccg
+    me: User = client.users.get_user_me()
+
+    assert me.id is not None
+    assert me.type == "user" or me.type == "enterprise"
+    assert me.name is not None
+    assert me.login is not None
